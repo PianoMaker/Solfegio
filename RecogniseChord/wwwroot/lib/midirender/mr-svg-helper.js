281ca9d343e87797f37fb2b27b561eb7d13db480
Andriy Bondarenko <bondareandre@gmail.com>
2026-08-27 15:44:27 +0300

    mr-svg-helper added scaleContext

---
+++ RecogniseChord/wwwroot/lib/midirender/mr-svg-helper.js
@@ -0,0 +1,9 @@
+﻿function scaleContext(scaleFactor, context) {
+    try {
+        if (scaleFactor !== 1 && typeof context.scale === 'function') {
+            context.scale(scaleFactor, scaleFactor);
+        }
+    } catch (e) {
+        console.warn("Scaling failed:", e);
+    }
+}
